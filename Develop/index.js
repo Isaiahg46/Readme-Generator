81c4f6fe10d4a8a7a673daf2bdcf6ac25e965cc5
Isaiah Gonzales <isaiahg46@gmail.com>
2024-11-20 16:55:23 -0800

    added code

---
+++ Develop/index.js
@@ -0,0 +1,71 @@
+// TODO: Include packages needed for this application
+import inquirer from 'inquirer';
+import fs from 'fs';
+import generateMarkdown from './utils/generateMarkdown.js';
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a description of your project.'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please provide installation instructions.'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please provide usage information.'
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Please provide contribution guidelines.'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Please provide test instructions.'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Please select a license.',
+        choices: ['MIT', 'GNU GPLv3', 'Apache 2.0', 'ISC', 'None']
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+    }
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.error(err) : console.log('Success!')
+    );
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions).then((response) => {
+        writeToFile('README.md', generateMarkdown(response))
+    });
+    
+};
+
+// Function call to initialize app
+init();
---
+++ Develop/utils/generateMarkdown.js
@@ -0,0 +1,85 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  if (license === 'none') {
+    return '';
+  } else {
+    return `![License](https://img.shields.io/badge/license-${license}-blue.svg)`;
+  }
+};
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+  /*if (license === 'none') {
+    return '';
+  } else {
+    return `[License](#license)`;
+  }*/
+ switch (license) {
+    case 'MIT':
+      return `[MIT](https://opensource.org/licenses/MIT)`;
+    case 'GNU GPLv3':
+      return `[GNU GPLv3](https://www.gnu.org/licenses/gpl-3.0)`;
+    case 'Apache 2.0':
+      return `[Apache 2.0](https://www.apache.org/licenses/LICENSE-2.0)`;
+    case 'ISC':
+      return `[ISC](https://opensource.org/licenses/ISC)`;
+    default:
+      return '';
+    case 'None':
+      return '';
+  }
+}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  if (license === 'none') {
+    return '';
+  } else {
+    return `## License
+    This project is licensed under the ${license} license.`;
+  }
+}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+${renderLicenseBadge(data.license)}
+
+## Description
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [License](#license)
+- [Questions](#questions)
+
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## Contributing
+${data.contribution}
+
+## Tests
+${data.test}
+
+${renderLicenseSection(data.license)}
+${renderLicenseLink(data.license)}
+
+## Questions
+[GitHub]${data.github}\n
+[Email]${data.email}
+
+`;
+}
+
+export default generateMarkdown;
